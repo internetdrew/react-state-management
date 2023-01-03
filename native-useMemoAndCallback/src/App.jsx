9af8384e4e484eea38b424fbf7f8c67d6b7f517a
Andrew Rowley <andrew@internetdrew.com>
2023-01-03 17:14:32 -0500

    break down useMemo

--- native-useMemoAndCallback/src/App.jsx
+++ native-useMemoAndCallback/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useMemo } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [numbers] = useState([10, 20, 30]);
+
+  const total = useMemo(
+    () => numbers.reduce((acc, number) => acc + number, 0),
+    [numbers]
+  );
+
+  // useMemo for complex calculations you
+  // don't want to re-render without changes
+
+  const [names] = useState(['John', 'Paul', 'George', 'Ringo']);
+
+  const sortedNames = useMemo(() => [...names].sort(), [names]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <div>Total: {total}</div>
+      <div>Names: {names.join(', ')}</div>
+      <div>Sorted names: {sortedNames.join(', ')}</div>
+    </>
+  );
 }
 
-export default App
+export default App;
