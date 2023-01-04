9d16d0b0a18d80158565438dbdf1e34de695073d
Andrew Rowley <andrew@internetdrew.com>
2023-01-03 23:38:33 -0500

    cover best practices for useEffect

--- native-useEffect/src/App.jsx
+++ native-useEffect/src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+
+const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log(time);
+      setTime(time => time + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div>Time: {time}</div>;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [names, setNames] = useState([]);
+
+  useEffect(() => {
+    fetch(`./names.json`)
+      .then(res => res.json())
+      .then(data => setNames(data));
+  }, []);
+
+  const [selectedName, setSelectedName] = useState(null);
+
+  const [selectedNameDetails, setSelectedNameDetails] = useState(null);
+
+  const onSelectedNameChange = name => {
+    fetch(`./${name}.json`)
+      .then(res => res.json())
+      .then(data => setSelectedNameDetails(data));
+  };
 
   return (
-    <div className="App">
+    <div>
+      <Stopwatch />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {names.map(name => (
+          <button key={name} onClick={() => onSelectedNameChange(name)}>
+            {name}
+          </button>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{selectedNameDetails && JSON.stringify(selectedNameDetails)}</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
