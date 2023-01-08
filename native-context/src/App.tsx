d94e9726925febccb759867d2bc2593f17234fb9
Andrew Rowley <andrew@internetdrew.com>
2023-01-08 07:23:03 -0500

    abstract provider

--- native-context/src/App.tsx
+++ native-context/src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { useState, useEffect, useContext, createContext } from 'react';
 import { PokemonProvider, usePokemon } from './store';
 
 const PokemonList = () => {
   const { pokemon } = usePokemon();
   return (
-    <div>
+    <ul className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-3'>
       {pokemon.map(p => (
-        <div key={p.id}>{p.name}</div>
+        <div key={p.id} className='text-center'>
+          {p.name}
+        </div>
       ))}
-    </div>
+    </ul>
   );
 };
 
@@ -17,7 +18,9 @@ function App() {
   return (
     <div>
       <PokemonProvider>
-        <PokemonList />
+        <div className='mx-auto max-w-3xl'>
+          <PokemonList />
+        </div>
       </PokemonProvider>
     </div>
   );
